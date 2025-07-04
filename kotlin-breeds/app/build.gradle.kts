873a1b2614b1062e71142545d370557e9b1d44c3
kikemadrigal <kikemadrigal@hotmail.com>
2025-07-04 06:57:31 +0200

    updated for API 36 compile

--- kotlin-breeds/app/build.gradle.kts
+++ kotlin-breeds/app/build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
+    //id ("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "es.tipolisto.breeds"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "es.tipolisto.breeds"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 4
-        versionName = "3.1"
+        targetSdk = 36
+        versionCode = 5
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -52,52 +54,52 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation(libs.androidx.core.ktx)
     //Corrutinas de las activities
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     //Aki detro está el image picker selector de fotos
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation(platform("androidx.compose:compose-bom:2024.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     // PERMISOS
-    implementation("com.google.accompanist:accompanist-permissions:0.28.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation(libs.accompanist.permissions)
+    implementation(libs.androidx.appcompat)
     //CameraX:https://developer.android.com/jetpack/androidx/releases/camera?hl=es-419
     // CameraX core library using the camera2 implementation
     //val camerax_version = "1.4.0-alpha04"
-    val camerax_version = "1.4.0-rc01"
+    //val cameraxVersion = "1.4.2"
     // The following line is optional, as the core library is included indirectly by camera-camera2
-    implementation("androidx.camera:camera-core:${camerax_version}")
-    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
     // If you want to additionally use the CameraX Lifecycle library
-    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation(libs.androidx.camera.lifecycle)
     // If you want to additionally use the CameraX VideoCapture library
-    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation(libs.androidx.camera.video)
     // If you want to additionally use the CameraX View class
-    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation(libs.androidx.camera.view)
     // If you want to additionally add CameraX ML Kit Vision Integration
     //implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
     // If you want to additionally use the CameraX Extensions library
-    implementation("androidx.camera:camera-extensions:${camerax_version}")
+    implementation(libs.androidx.camera.extensions)
     //Para recortar la imagen del animal en beauties
     //implementation("com.vanniktech:android-image-cropper:4.6.0")
 
     //LiveData para engancharse desde el composable screen al viewModel
     //Si no lo pones, esto no los puedes hacer: val email: String by loginViewModel.email.observeAsState(initial = "")
-    implementation ("androidx.compose.runtime:runtime-livedata:1.6.8")
+    implementation (libs.androidx.runtime.livedata)
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     //implementation ("com.squareup.okhttp3:logging-interceptor:4.4.0")
     //implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     //implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     //implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
 
     //Iconos a tope
-    implementation ("androidx.compose.material:material-icons-extended:1.6.8")
+    implementation (libs.androidx.material.icons.extended)
 
     //Tabs
     //implementation("com.google.accompanist:accompanist-pager:0.20.0")
@@ -110,7 +112,7 @@ dependencies {
     //Recuerda poner en el manifest: <uses-permission android:name="android.permission.INTERNET"/>
     //La librería de coil nos va a permitir cargar imágenes de forma muy sencilla desde internet
     //Coil: https://github.com/coil-kt/coil#jetpack-compose
-    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation(libs.coil.compose)
 
     //Navegación:https://developer.android.com/codelabs/jetpack-compose-navigation?hl=es-419#3
     /*
@@ -150,8 +152,9 @@ dependencies {
         }
       ){}
      */
-    val nav_version = "2.7.7"
-    implementation("androidx.navigation:navigation-compose:$nav_version")
+    //val navVersion = "2.9.0"
+    //noinspection GradleDependency
+    implementation(libs.androidx.navigation.compose)
 
     //Room
     //https://developer.android.com/training/data-storage/room?hl=es-419#kts
@@ -168,21 +171,22 @@ dependencies {
          y un navManager para la navegación
          NavManager(viewModel = viewModel)
      */
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
+    //val roomersion = "2.7.2"
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.room.compiler)
     // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")
+    //kapt("androidx.room:room-compiler:$room_version")
+    ksp(libs.room.compiler)
     // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:$room_version")
+    implementation(libs.androidx.room.ktx)
 
 
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation(libs.junit)
+    //androidTestImplementation(libs.androidx.junit)
+    //androidTestImplementation(libs.androidx.espresso.core)
+    //androidTestImplementation(platform(libs.androidx.compose.bom))
+    //androidTestImplementation(libs.androidx.ui.test.junit4)
+    //debugImplementation(libs.androidx.ui.tooling)
+    //debugImplementation(libs.androidx.ui.test.manifest)
 }
